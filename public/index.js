61a432d4f7a1c85bcc098885e5ea54b798b93be2
Greg Wright <gregwrightbjj@gmail.com>
2015-03-12 08:53:29 -0400

    finished

--- public/index.js
+++ public/index.js
@@ -2,14 +2,25 @@ var x = function(){}
 var templates = {}
 var views = {}
 
+var createDate = function(){
+  var date = moment().format("ll");
+  $(".date").html(date);
+}
 
+
+// var clock = $('.your-clock').FlipClock(3600, {
+// // ... your options here
+// 	clockFace: "TwelveHourClock"
+
+// });
+// var time  = clock.getTime();
 
 //Router
 var Router = Backbone.Router.extend({
 
 	routes:{
 		"": "tableOfContents",
-		"/page/:pageNumber":"specificPage"
+		"page/:pageNumber":"specificPage"
 	},
 
 	tableOfContents: function(){
@@ -25,9 +36,10 @@ var Router = Backbone.Router.extend({
 
 	specificPage: function(pageNumber){
 		$.ajax({
-			url:"/api/page/:" + pageNumber,
+			url:"/api/page/:" +pageNumber,
 			method: "GET",
 			success:function(data){
+				console.log(data)
 				views.pages.update(data)
 				$("#stuff").html(views.pages.$el)
 			}
@@ -41,7 +53,7 @@ var router = new Router()
 
 //table of contents
 var TocView = Backbone.View.extend({
-
+	className: "bigT",
 	initialize: function(){
 		this.data = []
 		this.render()
@@ -61,9 +73,10 @@ var TocView = Backbone.View.extend({
 
 //Pages view
 var PagesView = Backbone.View.extend({
+	className: "bigP",
 
 	initialize: function(){
-		this.data = []
+		this.data = {}
 		this.render()
 	},
 	
@@ -86,12 +99,16 @@ $(document).on("ready", function(){
 		Pages: Handlebars.compile($("#pages-template").text() )
 
 	}
+	
 	//Pages view instance
 	views.pages = new PagesView()
 	
 	//Toc view instance 
 	views.toc = new TocView() 
 	var router = new Router()
+
+	createDate()
+
 	
 	Backbone.history.start()
 
